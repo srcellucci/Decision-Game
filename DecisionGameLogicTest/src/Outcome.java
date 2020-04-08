8115868a51563b85823fac0eddc8f597d9d5ed40
Sara Cellucci <celluc44@students.rowan.edu>
2020-04-08 01:10:43 -0400

    Finished text-based logic test

--- DecisionGameLogicTest/src/Outcome.java
+++ DecisionGameLogicTest/src/Outcome.java
@@ -1,38 +1,68 @@
-import java.io.IOException;
 
-public class Outcome {
-
-	String plainText;
-	//String altText
-	double goodness;
-	double probability;
+public class Outcome 
+{
 	
+	private String text;
+	private double goodness;
+	private double probability;
 	
-	public String getPlainText() {
-		return plainText;
+	public Outcome() 
+	{
+		
 	}
 	
-	public void setPlainText(String plainText) {
-		this.plainText = plainText;
+	public Outcome(String text, double goodness, double probability)
+	{
+		this.text        = text;
+		this.goodness    = goodness;
+		this.probability = probability;
 	}
-	
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @return the goodness
+	 */
 	public double getGoodness() {
 		return goodness;
 	}
 	
-	public void setGoodness(double goodness) throws IOException {
-		if (goodness <= 10.0 && goodness >= -10.0) this.goodness = goodness;
-		else throw new IOException();
-	}
-	
+	/**
+	 * @return the probability
+	 */
 	public double getProbability() {
 		return probability;
 	}
 	
-	public void setProbability(double probability) throws IOException {
-		if (probability <= 1.0 && probability > 0.0) this.probability = probability;
-		else throw new IOException();
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * @param goodness the goodness to set
+	 */
+	public void setGoodness(double goodness) {
+		this.goodness = goodness;
+	}
+
+	/**
+	 * @param probability the probability to set
+	 */
+	public void setProbability(double probability) {
+		this.probability = probability;
 	}
 	
-	
+	public String toString()
+	{
+		return text + " : " + goodness + " : " + probability;
+	}
+
 }
